f3521050c5b8460f24661740bd5fc3f2c75a255d
Ridham <ridhampatel2002@gmail.com>
2024-02-29 15:39:04 +0530

    New

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>Ridham Patel</code> From React App.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
